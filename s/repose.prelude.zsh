387b5cf527cab0afee8970334c1a0a024706045b
Roman Neuhauser <neuhauser@sigpipe.cz>
2016-07-20 15:09:37 +0200

    license && copyright notices

--- s/repose.prelude.zsh
+++ s/repose.prelude.zsh
@@ -1,5 +1,20 @@
 # vim: ft=zsh sw=2 sts=2 et fdm=marker cms=\ #\ %s
 
+# Copyright (C) 2016 SUSE LLC
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 setopt extended_glob
 setopt hist_subst_pattern
 setopt err_return
