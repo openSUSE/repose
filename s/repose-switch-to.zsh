7da81821de19d0d764a333a6b5c46308836263d0
Roman Neuhauser <neuhauser@sigpipe.cz>
2016-07-20 15:09:37 +0200

    license && copyright notices

--- s/repose-switch-to.zsh
+++ s/repose-switch-to.zsh
@@ -1,6 +1,21 @@
 #!/usr/bin/zsh -f
 # vim: ft=zsh sw=2 sts=2 et fdm=marker cms=\ #\ %s
 
+# Copyright (C) 2016 SUSE LLC
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 declare -gr cmdname=$0:t
 
 declare -gr cmdhelp=$'
