e2e83079521484076e72b18f4cc1925ce685d656
Ondřej Súkup <osukup@suse.cz>
2016-11-10 15:57:12 +0100

    Added verbose mode for 'switch-to' command

--- src/repose-switch-to.zsh
+++ src/repose-switch-to.zsh
@@ -22,13 +22,14 @@ declare -gr cmdhelp=$'
 usage: #c -h | --help | [-n] HOST... -- REPA...
 Enable matching repositories, disable their complementary set
   Options:
-    -h                    Display this message
-    --help                Display full help
-    -n,--print            Display, do not perform destructive commands
+    -h                     Display this message
+    --help                 Display full help
+    -n,--print             Display, do not perform destructive commands
+    -v, --verbose          Enable verbose mode for scp,ssh commands
 
   Operands:
-    HOST                  Machine to operate on
-    REPA                  Repository pattern
+    HOST                   Machine to operate on
+    REPA                   Repository pattern
 '
 
 . ${REPOSE_PRELUDE:-@preludedir@/repose.prelude.zsh} || exit 2
