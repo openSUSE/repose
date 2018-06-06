3d190f5ae9f6ed4db28e8ba3a5d66da13ed47bbd
Jan Baier <jbaier@suse.cz>
2018-06-06 14:13:09 +0200

    Change help message and version bump

--- repose/argparsing.py
+++ repose/argparsing.py
@@ -64,14 +64,14 @@ def get_parser():
     """
 
     parser = argparse.ArgumentParser(
-        description="repository manipulation tool for QAM", prog="repose")
+        description="Repository manipulation tool for QAM", prog="repose")
 
     parser.add_argument(
         "-n",
         "--print",
         dest='dry',
         action="store_true",
-        help="Print commnads for hosts, and exit")
+        help="print commands for host and exit")
     parser.add_argument(
         "-V",
         "--version",
@@ -82,7 +82,7 @@ def get_parser():
         "--config",
         action="store",
         type=Path,
-        help="Patch to repose config",
+        help="path to repose configuration",
         default=Path('/etc/repose/products.yml'))
     group = parser.add_mutually_exclusive_group()
     group.add_argument(
@@ -100,7 +100,7 @@ def get_parser():
 
     # command ADD
 
-    cmdadd = commands.add_parser('add', help='Adds specified repos to targets')
+    cmdadd = commands.add_parser('add', help='add specified repository to target')
     cmdadd.add_argument(
         "-t",
         "--target",
@@ -108,7 +108,7 @@ def get_parser():
         type=ParseHosts,
         action="append",
         required=True,
-        help='Target to operate on')
+        help='target to operate on')
     cmdadd.add_argument(
         "repa",
         metavar="REPA",
@@ -121,7 +121,7 @@ def get_parser():
 
     cmdremove = commands.add_parser(
         'remove',
-        help='Removes repositories from targets')
+        help='remove repository from target')
     cmdremove.add_argument(
         "-t",
         "--target",
@@ -129,7 +129,7 @@ def get_parser():
         type=ParseHosts,
         action='append',
         required=True,
-        help="Target to operate on")
+        help="target to operate on")
     cmdremove.add_argument("repa", metavar="REPA", type=Repa, nargs="+")
     cmdremove.set_defaults(func=do_remove)
 
@@ -137,7 +137,7 @@ def get_parser():
 
     cmdreset = commands.add_parser(
         'reset',
-        help="Reset target repos to only installed products repositories")
+        help="reset target repositories to only installed products repositories")
     cmdreset.add_argument(
         "-t",
         "--target",
@@ -145,14 +145,14 @@ def get_parser():
         type=ParseHosts,
         action='append',
         required=True,
-        help="Target to operate on")
+        help="target to operate on")
     cmdreset.set_defaults(func=do_reset)
 
     # command INSTALL
 
     cmdinstall = commands.add_parser(
         'install',
-        help='Adds specified repos from target and install product')
+        help='add specified repository to target and install product')
     cmdinstall.add_argument(
         "-t",
         "--target",
@@ -160,7 +160,7 @@ def get_parser():
         type=ParseHosts,
         action='append',
         required=True,
-        help="Target to operate on")
+        help="target to operate on")
     cmdinstall.add_argument("repa", metavar="REPA", type=Repa, nargs="+")
     cmdinstall.set_defaults(func=do_install)
 
@@ -168,7 +168,7 @@ def get_parser():
 
     cmdclear = commands.add_parser(
         'clear',
-        help="Clear all repositories from target")
+        help="clear all repositories from target")
     cmdclear.add_argument(
         "-t",
         "--target",
@@ -176,14 +176,14 @@ def get_parser():
         type=ParseHosts,
         action='append',
         required=True,
-        help="Target to operate on")
+        help="target to operate on")
     cmdclear.set_defaults(func=do_clear)
 
     # command Unistall
 
     cmduninstall = commands.add_parser(
         'uninstall',
-        help='Removes specified repos to target and uninstall product')
+        help='remove specified repository from target and uninstall product')
     cmduninstall.add_argument(
         "-t",
         "--target",
@@ -191,7 +191,7 @@ def get_parser():
         type=ParseHosts,
         action='append',
         required=True,
-        help="Target to operate on")
+        help="target to operate on")
     cmduninstall.add_argument("repa", metavar="REPA", type=Repa, nargs="+")
     cmduninstall.set_defaults(func=do_uninstall)
 
@@ -199,7 +199,7 @@ def get_parser():
 
     cmdlistp = commands.add_parser(
         'list-products',
-        help="List products on target")
+        help="list products on target")
     cmdlistp.add_argument(
         "-t",
         "--target",
@@ -207,14 +207,14 @@ def get_parser():
         type=ParseHosts,
         action='append',
         required=True,
-        help="Target to operate on")
+        help="target to operate on")
     cmdlistp.set_defaults(func=do_list_products)
 
     # command LIST-Repos
 
     cmdlistr = commands.add_parser(
         'list-repos',
-        help="List repositories on target")
+        help="list repositories on target")
     cmdlistr.add_argument(
         "-t",
         "--target",
@@ -222,14 +222,14 @@ def get_parser():
         type=ParseHosts,
         action='append',
         required=True,
-        help="Target to operate on")
+        help="target to operate on")
     cmdlistr.set_defaults(func=do_list_repos)
 
     # command KnownProducts
 
     cmdknown = commands.add_parser(
         'known-products',
-        help="List known products by 'repose'")
+        help="list known products by 'repose'")
     cmdknown.set_defaults(func=do_known_products)
 
     return parser
