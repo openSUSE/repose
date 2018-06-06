c6c788a33b3f087989661beb2137adf7a6163eb4
Jan Baier <jbaier@suse.cz>
2018-06-06 13:32:36 +0200

    Catch errors from solve_repa
    Handle REPA errors more gracefully as requested in bsc#1096001

--- repose/command/install.py
+++ repose/command/install.py
@@ -17,7 +17,10 @@ class Install(Command):
         for target in self.targets.keys():
             repositories = {}
             for repa in self.repa:
+              try:
                 repositories.update(repoq.solve_repa(repa, self.targets[target].products.get_base()))
+              except ValueError as error:
+                logger.error(error)
 
             for repo in chain.from_iterable(x for x in (y for y in repositories.values())):
                 addcmd = self.addcmd.format(name=repo.name, url=repo.url, params="-cfkn" if repo.refresh else "-ckn")
@@ -28,11 +31,14 @@ class Install(Command):
                     self._report_target(target)
                     self.targets[target].run(self.refcmd)
 
-            inscmd = self.ipdcmd.format(products=" ".join(repositories.keys()))
-            if self.dryrun:
-                print(blue(str(target)) + " - {}".format(inscmd))
+            if repositories.keys():
+              inscmd = self.ipdcmd.format(products=" ".join(repositories.keys()))
+              if self.dryrun:
+                  print(blue(str(target)) + " - {}".format(inscmd))
+              else:
+                  self.targets[target].run(inscmd)
+                  self._report_target(target)
             else:
-                self.targets[target].run(inscmd)
-                self._report_target(target)
+              logger.error("No products to install")
 
         self.targets.close()
