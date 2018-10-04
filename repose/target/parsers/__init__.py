9468573f53c8e571d310622051bc9582d7b101af
Ondřej Súkup <osukup@suse.cz>
2018-10-04 13:05:47 +0200

    Reformat with Black

--- repose/target/parsers/__init__.py
+++ repose/target/parsers/__init__.py
@@ -1,5 +1,5 @@
 
 from collections import namedtuple
 
-Product = namedtuple('Product', ('name', 'version', 'arch'))
-Repository = namedtuple('Repository', ('alias','name', 'url', 'state'))
+Product = namedtuple("Product", ("name", "version", "arch"))
+Repository = namedtuple("Repository", ("alias", "name", "url", "state"))
