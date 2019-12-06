ef34e7b345a641c3d3d0c8e82bf0d5d713b84f94
Ondřej Súkup <mimi.vx@gmail.com>
2019-12-06 17:06:19 +0100

    Remove qamlib from requires

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
+
 from repose import __version__
 
 setup(
     name="repose",
     version=__version__,
-    packages=find_packages(exclude=['docs', 'tests*']),
-    install_requires=['paramiko', 'qamlib'],
-    tests_require=['pytest'],
-    entry_points={'console_scripts': ['repose = repose.main:main']},
-
+    packages=find_packages(exclude=["docs", "tests*"]),
+    install_requires=["paramiko"],
+    tests_require=["pytest"],
+    entry_points={"console_scripts": ["repose = repose.main:main"]},
     author="Ondřej Súkup",
-    author_email="osukup@suse.cz")
+    author_email="osukup@suse.cz",
+)
