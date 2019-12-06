fe969db2355f1753a020b99efbe784332ce892e1
Ondřej Súkup <mimi.vx@gmail.com>
2019-12-06 17:05:38 +0100

    Remove qamlib dep -  timestamp

--- repose/utils.py
+++ repose/utils.py
@@ -1,14 +1,26 @@
-
-
 import os
+import time
 
-if os.getenv('COLOR', 'always') == 'always':
-    def green(xs): return "\033[1;32m{!s}\033[1;m".format(xs)
 
-    def red(xs): return "\033[1;31m{!s}\033[1;m".format(xs)
+def timestamp():
+    # remove fractional part
+    return str(int(time.time()))
 
-    def yellow(xs): return "\033[1;33m{!s}\033[1;m".format(xs)
 
-    def blue(xs): return "\033[1;34m{!s}\033[1;m".format(xs)
+if os.getenv("COLOR", "always") == "always":
+
+    def green(xs):
+        return "\033[1;32m{!s}\033[1;m".format(xs)
+
+    def red(xs):
+        return "\033[1;31m{!s}\033[1;m".format(xs)
+
+    def yellow(xs):
+        return "\033[1;33m{!s}\033[1;m".format(xs)
+
+    def blue(xs):
+        return "\033[1;34m{!s}\033[1;m".format(xs)
+
+
 else:
     green = red = yellow = blue = lambda xs: str(xs)
